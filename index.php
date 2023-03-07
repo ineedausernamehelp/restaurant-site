02f340b482707f190fa55b9c9d09de943641d8ec
ineedausernamehelp <54716860+ineedausernamehelp@users.noreply.github.com>
2023-03-07 12:39:09 +0100

    main afgemaakt

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,20 +9,24 @@
     <link rel="stylesheet" href="styling/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;1,300;1,400&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;1,300;1,400&display=swap"
+        rel="stylesheet">
 </head>
+
 <body>
     <header>
         <img src="img/logo.png" alt="logo kan niet geladen worden" class="nav-text">
         <nav>
             <a href="pages/menu.php" class="nav-links">Menu</a>
             <a href="pages/contact.php" class="nav-links">Contact</a>
-        </nav>   
-    </header>   
+        </nav>
+    </header>
     <main>
-        <div class="main-img main-image-flex">
-            <h1 class="main-text welkom-tekst">Welkom</h1>
-            <p class="main-text beschrijving-tekst">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+        <div class="main-img-flex opacity">
+            <img src="img/food.jpg" alt="alt" class="main-img">
+            <h1 class="main-text welkom-tekst" >Welkom</h1>
+            <p class="main-text beschrijving-tekst">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor.</p>
             <div class="main-text main-knop">
                 <a href="pages/menu.php" class="main-knop-tekst">Bekijk ons menu</a>
             </div>
@@ -31,4 +36,5 @@
         <img src="img/footer-img.png" alt="footer image kan niet geladen worden" class="footer-img">
     </footer>
 </body>
-</html> 
+
+</html>
