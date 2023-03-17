e3d2a2219a39a064a78645afb35e1b98f02f8234
ineedausernamehelp <54716860+ineedausernamehelp@users.noreply.github.com>
2023-03-17 14:20:14 +0100

    menu blokken in php toegevoegd

--- menu.php
+++ menu.php
@@ -14,87 +14,14 @@
 </head>
 
 <body>
-    <?php include_once('header.php'); ?>
+    <?php include_once('header.php');
 
-    <main class="main-wrapper">
-        <div class=" menu-block">
-            <img src="https://images.newyorkpizza.nl/Products/Original/Brooklyn-8047.png" alt="" class="menu-foto">
-            <div class="menu-tekst">
-                <h2 class="gerechtnaam">Pizza</h2>
-                <div class="prijs-reviews-menu-tekst">
-                    <p>12.50$</p>
-                    <p>7/10</p>
-                </div>
-                <p class="beschrijving-gerecht-tekst">De pizza is een Italiaans gerecht. Het bestaat uit een bodem van
-                    deeg, de pizzabodem, waarop voedingsmiddelen als kaas of analoogkaas, tomaten, olijven, champignons,
-                    groenten en vlees gedaan worden.</p>
-                <a href="" class="winkelmand-tekst">toevoegen aan winkelmandje <span
-                        class="winkelmand-tekst plus">&nbsp;+</span></a>
-            </div>
-        </div>
-        <div class=" menu-block">
-            <img src="https://images.newyorkpizza.nl/Products/Original/Brooklyn-8047.png" alt="" class="menu-foto">
-            <div class="menu-tekst">
-                <h2 class="gerechtnaam">Pizza</h2>
-                <div class="prijs-reviews-menu-tekst">
-                    <p>12.50$</p>
-                    <p>7/10</p>
-                </div>
-                <p class="beschrijving-gerecht-tekst">De pizza is een Italiaans gerecht. Het bestaat uit een bodem van
-                    deeg, de pizzabodem, waarop voedingsmiddelen als kaas of analoogkaas, tomaten, olijven, champignons,
-                    groenten en vlees gedaan worden.</p>
-                <a href="" class="winkelmand-tekst">toevoegen aan winkelmandje <span
-                        class="winkelmand-tekst plus">&nbsp;+</span></a>
-            </div>
-        </div>
-        <div class=" menu-block">
-            <img src="https://images.newyorkpizza.nl/Products/Original/Brooklyn-8047.png" alt="" class="menu-foto">
-            <div class="menu-tekst">
-                <h2 class="gerechtnaam">Pizza</h2>
-                <div class="prijs-reviews-menu-tekst">
-                    <p>12.50$</p>
-                    <p>7/10</p>
-                </div>
-                <p class="beschrijving-gerecht-tekst">De pizza is een Italiaans gerecht. Het bestaat uit een bodem van
-                    deeg, de pizzabodem, waarop voedingsmiddelen als kaas of analoogkaas, tomaten, olijven, champignons,
-                    groenten en vlees gedaan worden.</p>
-                <a href="" class="winkelmand-tekst">toevoegen aan winkelmandje <span
-                        class="winkelmand-tekst plus">&nbsp;+</span></a>
-            </div>
-        </div>
-        <div class=" menu-block">
-            <img src="https://images.newyorkpizza.nl/Products/Original/Brooklyn-8047.png" alt="" class="menu-foto">
-            <div class="menu-tekst">
-                <h2 class="gerechtnaam">Pizza</h2>
-                <div class="prijs-reviews-menu-tekst">
-                    <p>12.50$</p>
-                    <p>7/10</p>
-                </div>
-                <p class="beschrijving-gerecht-tekst">De pizza is een Italiaans gerecht. Het bestaat uit een bodem van
-                    deeg, de pizzabodem, waarop voedingsmiddelen als kaas of analoogkaas, tomaten, olijven, champignons,
-                    groenten en vlees gedaan worden.</p>
-                <a href="" class="winkelmand-tekst">toevoegen aan winkelmandje <span
-                        class="winkelmand-tekst plus">&nbsp;+</span></a>
-            </div>
-        </div>
-        <div class=" menu-block">
-            <img src="https://images.newyorkpizza.nl/Products/Original/Brooklyn-8047.png" alt="" class="menu-foto">
-            <div class="menu-tekst">
-                <h2 class="gerechtnaam">Pizza</h2>
-                <div class="prijs-reviews-menu-tekst">
-                    <p>12.50$</p>
-                    <p>7/10</p>
-                </div>
-                <p class="beschrijving-gerecht-tekst">De pizza is een Italiaans gerecht. Het bestaat uit een bodem van
-                    deeg, de pizzabodem, waarop voedingsmiddelen als kaas of analoogkaas, tomaten, olijven, champignons,
-                    groenten en vlees gedaan worden.</p>
-                <a href="" class="winkelmand-tekst">toevoegen aan winkelmandje <span
-                        class="winkelmand-tekst plus">&nbsp;+</span></a>
-            </div>
-        </div>
 
-    </main>
-    <?php include_once('footer.php'); ?>
+    include('menu-block.php'); 
+    include('menu-block.php'); 
+    
+
+    include_once('footer.php'); ?>
 
 
 </body>
